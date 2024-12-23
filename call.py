c352ad5da7d27ca4cb174bf63bab7e341dfc02c6
Anuja Tappeta <153365922+anujatappeta@users.noreply.github.com>
2024-12-23 15:32:07 +0530

    Add files via upload

---
+++ call.py
@@ -0,0 +1,4 @@
+import segno
+text="+917842323599"
+a=segno.make("tel:"+text)
+a.save("call.png")
---
+++ scan.py
@@ -0,0 +1,4 @@
+import segno
+text="https://www.geeksforgeeks.org/problems/search-in-a-row-wise-sorted-matrix/1"
+a=segno.make(text)
+a.save("gfgpotd.png")
